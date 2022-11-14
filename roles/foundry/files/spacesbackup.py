e108a99675c2de4ca241b8f82060d74b97c07661
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-14 19:22:14 +0000

    Adding tarfile member sanitization to extractall()

--- roles/foundry/files/spacesbackup.py
+++ roles/foundry/files/spacesbackup.py
@@ -93,7 +93,26 @@ class BackupClient(object):
         logging.info('Restoring data.')
         try:
             with tarfile.open('/tmp/restore.tar.gz', 'r:gz') as tar:
-                tar.extractall(path=self._directory)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=self._directory)
         except Exception as e:
             logging.error('Unable to unpack archive. Down present at /tmp/restore.tar.gz')
             raise e
